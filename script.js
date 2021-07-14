dd2d06ee7ca41e7d7792e16f3b8c7831b12baac4
meivinay <tanwarv96@gmail.com>
2021-07-14 18:45:29 +0530

    Created Intitial UI and added Small functionality

---
+++ script.js
@@ -0,0 +1,61 @@
+let rowContainer= document.querySelector(".row-container");
+let columnContainer= document.querySelector(".column-container");
+let cellSection=document.querySelector(".cell-section");
+let selectedCell = document.querySelector(".selected-cell");
+let dataObj={};
+let lastCell=undefined;
+//scroll
+cellSection.addEventListener("scroll",(e)=>{
+    columnContainer.style.transform=`translateX(-${e.currentTarget.scrollLeft}px)`;   
+    rowContainer.style.transform =`translateY(-${e.currentTarget.scrollTop}px)`;
+})
+//row serial number creation
+for(let i=1;i<=100;i++){
+    let div=document.createElement("div");   
+    div.innerText=i;
+    div.classList.add("row-number");
+    rowContainer.append(div);
+}
+//column name creation
+for(let i=0;i<26;i++){
+    let asciiCode=65+i;
+    let alphabet=String.fromCharCode(asciiCode);   
+    let div=document.createElement("div");
+    div.classList.add("column");
+    div.innerText=alphabet;
+    columnContainer.append(div);
+}
+//cell creation
+for(let i=1;i<=100;i++){
+    let row=document.createElement("div");      
+    row.classList.add("row");
+    for(let j=0;j<26;j++){
+        let asciiCode=65+j;
+        let alphabet=String.fromCharCode(asciiCode);
+        let cellAdress=alphabet+i;
+        let cell=document.createElement("div");
+        cell.classList.add("cell");
+        cell.setAttribute("data-address",cellAdress);
+        dataObj[cellAdress]={};
+        dataObj[cellAdress]["value"]=undefined;
+        dataObj[cellAdress]["downStream"]=[];
+        dataObj[cellAdress]["upStream"]=[];
+        cell.contentEditable="true"
+        row.append(cell);
+//attaching event listner to every cell
+        cell.addEventListener("click",(e)=>{   //remove lastSelected cell border and add border to newly selected cell
+            if(lastCell){
+                lastCell.classList.remove("cell-selected");
+            }
+            e.currentTarget.classList.add("cell-selected");
+            lastCell=e.currentTarget; 
+            let currCellAddress = e.currentTarget.getAttribute("data-address");
+            selectedCell.innerText =currCellAddress;
+            
+        })
+
+    }
+    cellSection.append(row);
+}
+
+console.log(dataObj);
