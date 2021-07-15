60ccfc138d7f4693ef5dacbc20dd56c83df66180
meivinay <tanwarv96@gmail.com>
2021-07-15 12:20:21 +0530

    update updateDownStream

--- script.js
+++ script.js
@@ -42,20 +42,10 @@ for (let i = 1; i <= 100; i++) {
             downStream: [],
             upStream: []
         };
-        // dataObj[cellAdress]["value"]=undefined;
-        // dataObj[cellAdress]["downStream"]=[];
-        // dataObj[cellAdress]["upStream"]=[];
+
         //stroing value to dataObject
         cell.addEventListener("input", (e) => {
-            let currCellAddress = e.currentTarget.getAttribute("data-address");
-            let currCellObj = dataObj[currCellAddress];
-            currCellObj.value = e.currentTarget.innerText;
-            currCellObj.formula=undefined;
-            let upStream=currCellObj.upStream;
-            for(let i=0;i<upStream.length;i++){
-                removeFromDownStream(upStream[i],currCellAddress);
-            }
-            dataObj[currCellAddress].upStream=[];
+            updateCell(e);
         })
         cell.contentEditable = "true";
         row.append(cell);
@@ -75,14 +65,61 @@ for (let i = 1; i <= 100; i++) {
     cellSection.append(row);
 }
 
-function removeFromDownStream(parentCell,childCell){
-    let parentDownStream=dataObj[parentCell].downStream;
-    let filterDownStream=[];
-    for(let i=0;i<parentDownStream.length;i++){
-        if(parentDownStream[i]!=childCell){
+function removeFromDownStream(parentCell, childCell) {
+    let parentDownStream = dataObj[parentCell].downStream;
+    let filterDownStream = [];
+    for (let i = 0; i < parentDownStream.length; i++) {
+        if (parentDownStream[i] != childCell) {
             filterDownStream.push(parentDownStream[i]);
         }
     }
 
-    dataObj[parentCell].downStream=filterDownStream;
+    dataObj[parentCell].downStream = filterDownStream;
+}
+
+function updateDownStream(cell) {
+    let currCellObj = dataObj[cell];
+    let formula = currCellObj.formula;
+    let upStream = currCellObj.upStream;
+    let downStream = currCellObj.downStream;
+    // getting values of cell-address present in our formula form upstream array of our object
+    let valObj = {};
+    for (let i = 0; i < upStream.length; i++) {
+        let value = dataObj[upStream[i]].value;
+        valObj[upStream[i]] = value;
+    }
+    for (let key in valObj) {
+        formula.replace(key, valObj[key]);
+    }
+
+    let newValue = eval(formula);
+    dataObj.value = newValue;
+    for (let i = 0; i < downStream.length; i++) {
+        updateDownStream(downStream[i]);
+    }
+}
+
+function updateCell(e) {
+    // console.log(e.currentTarget.innerText);
+    let currCellAddress = e.currentTarget.getAttribute("data-address");
+    let currCellObj = dataObj[currCellAddress];
+    currCellObj.value = e.currentTarget.innerText;
+    currCellObj.formula = undefined;
+    
+    //work on upStream
+    //upsteam me jake apne app ko remove krvana hai, fir hr jgh se remove krvane ke baad apna upstream array empty kr dena hai
+    let upStream = currCellObj.upStream;
+    for (let i = 0; i < upStream.length; i++) {
+        removeFromDownStream(upStream[i], currCellAddress);
+    }
+    currCellObj.upStream = [];
+   
+    //work on downStream 
+    // apni downStream pe pde cell-address ko jaake btana hai ki meri value update hui hai , tum bhi meri value ko fetch kr ke update ho jao
+    let downStream = currCellObj.downStream;
+    for (let i = 0; i < downStream.length; i++) {
+        let currCellOfDownStream = downStream[i];
+        updateDownStream(currCellOfDownStream); //updated value ko pass krne ki zrurat nhi hai kyoki hum iss function me upstream me jaake fetch kr lege
+    }
+    console.log(dataObj);
 }
